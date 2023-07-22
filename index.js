683e0eb0a075659b5164443ad628b90c91c4f958
Michael Alan Stowe <mastowe94@gmail.com>
2023-07-22 13:38:09 -0500

    makes buttons that log the class on console

--- index.js
+++ index.js
@@ -1,14 +1,31 @@
 document.addEventListener("DOMContentLoaded",doAll)
 
-const url = "https://www.dnd5eapi.co/api"
-let classes = ""
+const url = "https://www.dnd5eapi.co/api/classes"
+// let classes = ""
+let selectedClassName = ""
 
-function doAll(){
+function doAll(){  //initializes once the DOM is loaded
     getClasses()
 }
 
-function getClasses(){
-    fetch(url)
+
+function getClasses(){ //fetches data to put through to make buttons 
+    fetch(url)         //to select the class information to reveal
     .then(resp => resp.json())
-    .then(data => classes = data)
+    .then(data => makeButtons(data.results))
 }
+
+function makeButtons(classes){
+    console.log(classes)
+    classes.map(element => {
+    fetch(`https://www.dnd5eapi.co/api/classes/${element.index}`)
+    .then(resp => resp.json())
+    .then(data => {
+        console.log(data.index)
+        const button = document.createElement("button")
+        button.innerText = data.index
+        button.addEventListener("click", () => {console.log(data.index)})
+        document.getElementById("buttons").appendChild(button)
+    })})
+}
+
